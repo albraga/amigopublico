3e5e11b64860a4dc725d8943a64f2eda09fa2564
82038198 <82038198@MPB26475.correiosnet.int>
2014-09-18 15:04:28 -0300

    addicionado icon me

--- public_html/js/amigopublico.js
+++ public_html/js/amigopublico.js
@@ -1,25 +1,59 @@
-var AmigoPublico = (function() {
+var AmigoPublico = (function () {
+
+    setMe = function (longi, lat) {
+        var iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.transform([longi, lat], 'EPSG:4326', 'EPSG:3857')),
+            name: 'Null Island',
+            population: 4000,
+            rainfall: 500
+        });
 
 
-getLocation = function() {
-if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(geoSuccess);
-    } 
-};
+        var iconStyle = new ol.style.Style({
+            image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+                anchor: [0.5, 46],
+                anchorXUnits: 'fraction',
+                anchorYUnits: 'pixels',
+                opacity: 0.75,
+                src: 'img/me.png'
+            }))
+        });
 
-geoSuccess = function(position) {
+        iconFeature.setStyle(iconStyle);
+
+        var vectorSource = new ol.source.Vector({
+            features: [iconFeature]
+        });
+
+        var vectorLayer = new ol.layer.Vector({
+            source: vectorSource
+        });
+        return vectorLayer;
+    }
+
+
+    getLocation = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(geoSuccess);
+        }
+    };
+
+    geoSuccess = function (position) {
+        var longi = position.coords.longitude;
+        var lat = position.coords.latitude;
+        var vectorLayer = setMe(longi, lat);
         var map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.MapQuest({layer: 'osm'})
+            target: 'map',
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.MapQuest({layer: 'osm'})
+                }), vectorLayer
+            ],
+            view: new ol.View({
+                center: ol.proj.transform([longi, lat], 'EPSG:4326', 'EPSG:900913'),
+                zoom: 16
             })
-        ],
-        view: new ol.View({
-            center: ol.proj.transform([position.coords.longitude, position.coords.latitude], 'EPSG:4326', 'EPSG:900913'),
-            zoom: 16
-        })
-    });
-};
-getLocation();
+        });
+    };
+    getLocation();
 })();
