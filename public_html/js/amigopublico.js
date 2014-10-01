e91d4504705fbcbd04f782d209202af673c5a8f2
AlBraga <>
2014-10-01 08:51:45 -0300

    createNeighbourhood()

--- public_html/js/amigopublico.js
+++ public_html/js/amigopublico.js
@@ -1,36 +1,38 @@
 var AmigoPublico = (function () {
 
-    setMe = function (longi, lat) {
+    createPOI = function (longi, lat, iconSRC) {
         var iconFeature = new ol.Feature({
             geometry: new ol.geom.Point(ol.proj.transform([longi, lat], 'EPSG:4326', 'EPSG:3857')),
-            name: 'Null Island',
-            population: 4000,
-            rainfall: 500
+            name: 'nome'
         });
-
-
         var iconStyle = new ol.style.Style({
             image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
                 anchor: [0.5, 46],
                 anchorXUnits: 'fraction',
                 anchorYUnits: 'pixels',
                 opacity: 0.75,
-                src: 'img/me.png'
+                src: iconSRC
             }))
         });
-
         iconFeature.setStyle(iconStyle);
-
         var vectorSource = new ol.source.Vector({
             features: [iconFeature]
         });
-
         var vectorLayer = new ol.layer.Vector({
             source: vectorSource
         });
         return vectorLayer;
-    }
+    };
 
+    createNeighbourhood = function (longi, lat) {
+        var layers = [];
+        layers[0] = new ol.layer.Tile({
+                    source: new ol.source.MapQuest({layer: 'osm'})
+                });
+        layers[1] = createPOI(longi, lat, 'img/user.png');
+        layers[2] = createPOI(longi + 0.01, lat, 'img/school.png');
+        return layers;
+    };
 
     getLocation = function () {
         if (navigator.geolocation) {
@@ -41,14 +43,10 @@ var AmigoPublico = (function () {
     geoSuccess = function (position) {
         var longi = position.coords.longitude;
         var lat = position.coords.latitude;
-        var vectorLayer = setMe(longi, lat);
+        var layersN = createNeighbourhood(longi, lat);
         var map = new ol.Map({
             target: 'map',
-            layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.MapQuest({layer: 'osm'})
-                }), vectorLayer
-            ],
+            layers: layersN,
             view: new ol.View({
                 center: ol.proj.transform([longi, lat], 'EPSG:4326', 'EPSG:900913'),
                 zoom: 16
